d4172c4dbf2702f81e033077a4179775a8b6a985
Victor Ferraz <victorfsf.dev@gmail.com>
2019-07-11 18:20:30 -0300

    Add more react rules

--- plugins/react.js
+++ plugins/react.js
@@ -8,6 +8,10 @@ module.exports = {
   },
   rules: {
     /* Errors */
+    'react/button-has-type': ['error'],
+    'react/default-props-match-prop-types': ['error', { allowRequiredDefaults: false }],
+    'react/destructuring-assignment': ['error', 'always'],
+    'react/forbid-foreign-prop-types': ['error'],
     'react/jsx-key': ['error'],
     'react/jsx-no-comment-textnodes': ['error'],
     'react/jsx-no-duplicate-props': ['error', { ignoreCase: false }],
@@ -15,10 +19,37 @@ module.exports = {
     'react/jsx-no-undef': ['error', { allowGlobals: false }],
     'react/jsx-uses-react': ['error'],
     'react/jsx-uses-vars': ['error'],
+    'react/no-access-state-in-setstate': ['error'],
     'react/no-children-prop': ['error'],
     'react/no-danger-with-children': ['error'],
+    'react/no-danger': ['error'],
+    'react/no-deprecated': ['error'],
+    'react/no-did-update-set-state': ['error'],
+    'react/no-direct-mutation-state': ['error'],
+    'react/no-find-dom-node': ['error'],
+    'react/no-is-mounted': ['error'],
+    'react/no-redundant-should-component-update': ['error'],
+    'react/no-render-return-value': ['error'],
+    'react/no-string-refs': ['error', { noTemplateLiterals: true }],
+    'react/no-this-in-sfc': ['error'],
+    'react/no-typos': ['error'],
+    'react/no-unescaped-entities': ['error'],
+    'react/no-unknown-property': ['error'],
+    'react/no-will-update-set-state': ['error'],
+    'react/prop-types': ['error'],
+    'react/react-in-jsx-scope': ['error'],
+    'react/require-render-return': ['error'],
     /* Disabled */
+    'react/boolean-prop-naming': ['off'], // TODO: Validate
     'react/display-name': ['off'],
+    'react/forbid-component-props': ['off'],
+    'react/forbid-dom-props': ['off'],
+    'react/forbid-elements': ['off'],
+    'react/forbid-prop-types': ['off'], // TODO: Validate
+    'react/no-array-index-key': ['off'], // TODO: Validate
+    'react/no-did-mount-set-state': ['off'],
+    'react/no-multi-comp': ['off'],
+    'react/no-set-state': ['off'],
     /* React Hooks */
     'react-hooks/rules-of-hooks': ['error'],
   },
